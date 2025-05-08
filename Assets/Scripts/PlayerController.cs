ea0c72a01b244c1a6fd53e471dee9ebed4253b53
DESKTOP-TAIMIDJ\FamBa <Fam.bauer@live.at>
2025-05-08 05:46:06 +0200

    minor updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,4 +4,40 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
 
+    PlayerControls playerControls;
+    Vector2 movement;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput() 
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+        Debug.Log(movement.x);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
 }
