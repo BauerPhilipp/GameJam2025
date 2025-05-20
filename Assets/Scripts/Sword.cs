d58783095aa6e74bbe03a429c1075665be62d149
DESKTOP-TAIMIDJ\FamBa <Fam.bauer@live.at>
2025-05-20 05:46:09 +0200

    3.3 finished

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+    }
+}
